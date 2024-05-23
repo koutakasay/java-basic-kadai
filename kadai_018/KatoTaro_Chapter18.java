575101efaafe20e475d7466c295b0d3ed7887463
k-takase <165891042+koutakasay@users.noreply.github.com>
2024-05-23 21:34:26 +0900

    Update KatoTaro_Chapter18.java

--- kadai_018/KatoTaro_Chapter18.java
+++ kadai_018/KatoTaro_Chapter18.java
@@ -6,17 +6,12 @@ public class KatoTaro_Chapter18 extends Kato_Chapter18 {
 		givenName = "太郎";
 	}
 	
-	public void commonIntroduce(String familyName, String taroGivenName, String address) {
-		System.out.println("名前は" + familyName + taroGivenName + "です");
-		System.out.println("住所は" + address + "です");
-	}
-	
 	public void eachIntroduce() {
 		System.out.println("Javaが得意です");
 	}
 	
 	public void execIntroduce() {
-		this.commonIntroduce("加藤", givenName, "東京都中野区〇×");
+		this.commonIntroduce();
 		this.eachIntroduce();
 	}
 }
