f5524e5cf963983a8d73a2c416ba7bf09df848f4
k-takase <165891042+koutakasay@users.noreply.github.com>
2024-05-29 09:20:03 +0900

    Update Dictionary_Chapter21.java

--- kadai_021/Dictionary_Chapter21.java
+++ kadai_021/Dictionary_Chapter21.java
@@ -22,18 +22,12 @@ public class Dictionary_Chapter21 extends DictionaryExec_Chapter21 {
 	
 	public void checkIndex() {
 		for(int i = 0; i < word.length; i++) {
-			switch(word[i]) {
-				case "apple" -> System.out.println("appleの意味は" + index.get("apple"));
-				case "peach" -> System.out.println("appleの意味は" + index.get("peach"));
-				case "banana" -> System.out.println("appleの意味は" + index.get("banana"));
-				case "lemon" -> System.out.println("appleの意味は" + index.get("lemon"));
-				case "pear" -> System.out.println("appleの意味は" + index.get("pear"));
-				case "kiwi" -> System.out.println("appleの意味は" + index.get("kiwi"));
-				case "strawberry" -> System.out.println("appleの意味は" + index.get("strawberry"));
-				case "grape" -> System.out.println("appleの意味は" + index.get("grape"));
-				case "muscat" -> System.out.println("appleの意味は" + index.get("muscat"));
-				case "cherry" -> System.out.println("appleの意味は" + index.get("cherry"));
-				default -> System.out.println(word[i] + "は辞書に存在しません");
+			String name = index.get(word[i]);
+			
+			if(!(name == null)) {
+				System.out.println(word[i] + "の意味は" + name);
+			} else {
+				System.out.println(word[i] + "は辞書には存在しません");
 			}
 		}
 	}
